ec908f8cb4897d1db94cc071e68efa6b27b0b19b
Diego Rios <riosdiego9418@gmail.com>
2023-03-20 12:02:20 -0300

    fix/lista_ordenadacao

--- estrutura.py
+++ estrutura.py
@@ -1,7 +1,5 @@
 elementos = [['a', 'b', 'c', 'd'], ['q', 'i', 'n', 'm'], ['f', 'e', 'h', 'j'], ['p', 'o', 'l','g'],];
 
-elementos2 = [elementos for j in elementos for elementos in j]
+elementos2 = sorted([elementos for j in elementos for elementos in j])
 
-reordenacao = sorted(elementos2)
-
-print(reordenacao)
+print(elementos2)
