28e48341739df57389bba286137ad67cb3d6a061
Diego Rios <riosdiego9418@gmail.com>
2023-03-21 13:33:29 -0300

    feature/BigO

--- estrutura.py
+++ estrutura.py
@@ -1,26 +1,29 @@
-elementos = [['a', 'b', 'c', 'd'], ['q', 'i', 'n', 'm'], ['f', 'e', 'h', 'j'], ['p', 'o', 'l','g'],];
+import matplotlib.pyplot as plt 
+matriz = [['a', 'b', 'c', 'd'],['q', 'i', 'n', 'm'],['f', 'e', 'h', 'j'], ['p', 'o', 'l', 'g']]
+
+
 #definimos as variaveis que utilizaremos para trabalhar com a matriz fornecida
-maLi = len(elementos)  #Linha
-maCol = len(elementos[0])  #Coluna
+maLi = len(matriz)  #Linha
+maCol = len(matriz[0])  #Coluna
 iniLi = 0   #Linha inicial
 fimLi = maLi - 1    #Define linha final pegando a o indice da matriz e subtraindo 1
-iniCol = 0  #Define a coluna inicial
+iniCol = 0  #Define a coluna inicial 
 fimCol = maCol - 1  #Define coluna final pegando a o indice da matriz e subtraindo 1
-indices = []   #array para atribuir as posições e fazer indexação com for.
+indices = []   #array para atribuir as posi��es e fazer indexa��o com for.
 
-# Abre laço
+# Abre laco
 while iniLi <= fimLi and iniCol <= fimCol:
     # Percorre a linha superior da matriz
     for i in range(iniCol, fimCol + 1):
         indices.append(f"{iniLi}:{i}")
     iniLi += 1
 
-    # Percorre a última coluna da matriz
+    # Percorre a ultima coluna da matriz
     for i in range(iniLi, fimLi + 1):
         indices.append(f"{i}:{fimCol}")
     fimCol -= 1
 
-    # Percorre a última linha da matriz
+    # Percorre a ultima linha da matriz
     if iniLi <= fimLi:
         for i in range(fimCol, iniCol - 1, -1):
             indices.append(f"{fimLi}:{i}")
@@ -33,18 +36,33 @@ while iniLi <= fimLi and iniCol <= fimCol:
         iniCol += 1
 
 # Ordena os elementos em ordem crescente e imprime
-matrizPronta = sorted([mtx for linha in elementos for mtx in linha])
+matrizPronta = sorted([mtx for linha in matriz for mtx in linha])
 print("Matriz ordenada: ", matrizPronta)
-print("Posições:", indices)
+print("Posi��es:", indices)
+
+
+x = list(range(len(matrizPronta)))
+y = [ord(c) for c in matrizPronta]
+
+plt.plot(x, y)
+plt.title("Grafico de Nota��o Big'O")
+plt.xlabel("Tamanho do conjunto de dados")
+plt.ylabel("Tempo de execu��o")
+plt.show()
 
 
 """
-Este código cria uma matriz de 4x4 com letras e, em seguida, ordena seus elementos em ordem crescente e imprime a matriz ordenada e as posições em que os elementos estão na matriz.
-O script começa definindo as variáveis que serão usadas para trabalhar com a matriz, como o número de linhas e colunas, as posições iniciais e
-finais e uma lista vazia para armazenar as posições dos elementos. Em seguida, um laço while é iniciado, que percorre a matriz em espiral,
-começando pela linha superior e percorrendo em sentido horário.
-Dentro do laço while, há quatro laços for aninhados, que percorrem a linha superior, a última coluna, a última linha e a primeira coluna da matriz, respectivamente.
-Para cada iteração desses laços, as posições dos elementos correspondentes são adicionadas à lista de índices.
-Finalmente, a lista de índices é impressa juntamente com a matriz ordenada, que é criada usando a função sorted() aplicada à matriz original.
+Claro! Para calcular a nota��o big'O' do algoritmo, precisamos analisar seu comportamento em rela��o ao tamanho de entrada.
 
+No caso desse algoritmo, o tamanho de entrada � o n�mero de elementos na matriz, que � dado por maLi * maCol.
+
+Se analisarmos o c�digo, percebemos que a complexidade do algoritmo � O(maLi * maCol), pois ele percorre cada elemento da matriz uma vez.
+
+Para exibir a nota��o big'O' em um gr�fico, precisamos plotar um gr�fico em que o eixo x representa o tamanho de entrada e o eixo y representa o tempo de execu��o.
+
+No c�digo acima, primeiro ordenamos a matriz para obter uma lista ordenada dos elementos. Em seguida, criamos duas listas x e y. A lista x cont�m valores de 0 a maLi * maCol - 1, que representam o tamanho do conjunto de dados. A lista y cont�m o tempo de execu��o para cada tamanho de entrada.
+
+No nosso caso, o tempo de execu��o � simplesmente o tempo necess�rio para ordenar a matriz, que � dado pelo m�todo sorted. Como a complexidade do algoritmo � O(maLi * maCol), esperamos que o tempo de execu��o cres�a proporcionalmente a maLi * maCol.
+
+Finalmente, usamos a biblioteca matplotlib para plotar um gr�fico de x versus y. A partir do gr�fico, podemos visualizar como o tempo de execu��o cresce em rela��o ao tamanho do conjunto de dados, e assim confirmar a nota��o big'O' do algoritmo.
 """
