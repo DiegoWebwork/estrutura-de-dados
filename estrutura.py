d56bd64d2b195d8cd4e954b03efbbeb56c59c160
DiegoWebwork <68088452+DiegoWebwork@users.noreply.github.com>
2023-03-21 13:35:39 -0300

    Update estrutura.py

--- estrutura.py
+++ estrutura.py
@@ -49,20 +49,3 @@ plt.title("Grafico de Nota��o Big'O")
 plt.xlabel("Tamanho do conjunto de dados")
 plt.ylabel("Tempo de execu��o")
 plt.show()
-
-
-"""
-Claro! Para calcular a nota��o big'O' do algoritmo, precisamos analisar seu comportamento em rela��o ao tamanho de entrada.
-
-No caso desse algoritmo, o tamanho de entrada � o n�mero de elementos na matriz, que � dado por maLi * maCol.
-
-Se analisarmos o c�digo, percebemos que a complexidade do algoritmo � O(maLi * maCol), pois ele percorre cada elemento da matriz uma vez.
-
-Para exibir a nota��o big'O' em um gr�fico, precisamos plotar um gr�fico em que o eixo x representa o tamanho de entrada e o eixo y representa o tempo de execu��o.
-
-No c�digo acima, primeiro ordenamos a matriz para obter uma lista ordenada dos elementos. Em seguida, criamos duas listas x e y. A lista x cont�m valores de 0 a maLi * maCol - 1, que representam o tamanho do conjunto de dados. A lista y cont�m o tempo de execu��o para cada tamanho de entrada.
-
-No nosso caso, o tempo de execu��o � simplesmente o tempo necess�rio para ordenar a matriz, que � dado pelo m�todo sorted. Como a complexidade do algoritmo � O(maLi * maCol), esperamos que o tempo de execu��o cres�a proporcionalmente a maLi * maCol.
-
-Finalmente, usamos a biblioteca matplotlib para plotar um gr�fico de x versus y. A partir do gr�fico, podemos visualizar como o tempo de execu��o cresce em rela��o ao tamanho do conjunto de dados, e assim confirmar a nota��o big'O' do algoritmo.
-"""
